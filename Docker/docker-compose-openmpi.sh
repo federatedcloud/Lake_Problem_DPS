e2e784bf0dde83222d21ef3727646ae52bd7731d
sk8forether <pzv2@cornell.edu>
2018-07-16 17:24:45 +0000

    Adding Docker OpenMPI template

---
+++ Docker/docker-compose-openmpi.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# Usage: source docker-compose-openmpi.sh <docker-compose argument list>
+#
+# This is a wrapper for docker-compose that performs
+# some variable substitution - just sue as you normally would
+# docker-compose, except for -f <file.yml>, which is already included
+#
+#
+# See BASEDIR/build.sh for a simpler way to build the docker image, without compose
+#
+
+#BASEDIR="Base/OpenMPI"
+BASEDIR="."
+export BASEDIR
+
+# shellcheck source=Base/OpenMPI/build.sh
+source "$BASEDIR/build.sh"
+docker-compose -f docker-compose-openmpi.yml "$@"
+
+
