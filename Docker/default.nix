df8536284b44a0e7cf344c543aac5c4c96509559
sk8forether <pzv2@cornell.edu>
2018-08-20 15:23:46 +0000

    Moved MOEAFramework to default.nix (tested)

--- Docker/default.nix
+++ Docker/default.nix
@@ -1,17 +1,54 @@
 with import <nixpkgs> {};
 # with pkgs.python36Packages;
+let
+  MOEAFramework = stdenv.mkDerivation {
+    name = "MOEAFramework";
+    
+    src = fetchurl {
+      url = https://github.com/MOEAFramework/MOEAFramework/releases/download/v2.12/MOEAFramework-2.12-Demo.jar;
+      sha256 = "0kmfdmpzpfl7f1hnfck7nyfimsd92d2fndsypfnfar6gqw5cl3w4";
+    };
+    phases = "installPhase";
+    
+    installPhase = ''
+      mkdir -p $out/
+      cp -v $src $out/MOEAFramework-2.12-Demo.jar
+    '';
+  };
+in
 stdenv.mkDerivation {
   name = "impurePythonEnv";
   buildInputs = [
+    nix
+    bash
+    
+    # MPI-related packages
+    binutils
+    gfortran
+    openmpi
+    openssh
+    
+    # python packages 
     python36Full
     python36Packages.mpi4py
     python36Packages.numpy    
     python36Packages.pip
     python36Packages.scipy
-    python36Packages.virtualenv    
+    python36Packages.virtualenv
+    
+    #
+    # Project code dependencies
+    #
+    MOEAFramework
+    
   ];
+  #postBuild = ''
+  #  echo "AAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHH" > /opt/diditwork.txt
+  #  cp ${MOEAFramework.out}/MOEAFramework-2.12-Demo.jar /opt
+  #'';
   src = null;
   shellHook = ''
     export LANG=en_US.UTF-8
+    cp ${MOEAFramework.out}/MOEAFramework-2.12-Demo.jar ~/
   '';
 }
--- Docker/dev-env.nix
+++ Docker/dev-env.nix
@@ -2,24 +2,8 @@ with import <nixpkgs> { };
 # For testing local nixpkgs clone, swap with above
 # with import ((builtins.getEnv "HOME") + "/workspace/nixpkgs") { }; # or:
 # with import "../nixpkgs" { };
-# Note taht the above are not accessible during docker build
+# Note that the above are not accessible during docker build
 
-let
-  MOEAFramework = stdenv.mkDerivation {
-    name = "MOEAFramework";
-    
-    src = fetchurl {
-      url = https://github.com/MOEAFramework/MOEAFramework/releases/download/v2.12/MOEAFramework-2.12-Demo.jar;
-      sha256 = "0kmfdmpzpfl7f1hnfck7nyfimsd92d2fndsypfnfar6gqw5cl3w4";
-    };
-    phases = "installPhase";
-    
-    installPhase = ''
-      mkdir -p $out/Lake_Problem_DPS/Optimization/
-      cp -v $src $out/Lake_Problem_DPS/Optimization/
-    '';
-  };
-in 
 { LakeProblemDPSDevEnv = buildEnv {
   name = "lake-problem-dps-dev-env";
   paths = [
@@ -37,11 +21,6 @@ in
     gfortran
     openmpi
     openssh
-    
-    #
-    # Project code dependencies
-    #
-    MOEAFramework
     
     ];
   
