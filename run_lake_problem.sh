923dc259b1ef2247de2e3bef6b897fcd789391c6
sk8forether <pzv2@cornell.edu>
2020-02-18 02:52:16 +0000

    Added some output text to show progress

--- run_lake_problem.sh
+++ run_lake_problem.sh
@@ -7,22 +7,28 @@ cd /home/nixuser/Lake_Problem_DPS/Optimization/DPS
 for i in {1..50}
 do
   mpirun ./LakeDPSparallel $i 2000 #00
+  echo "$i DPS iteration complete"
 done
 
-cd /home/nixuser/Lake_Problem_DPS/Optimization/Intertemporal/
+cd /home/nixuser/Lake_Problem_DPS/OptimizationIntertemporal/
 
 for i in {1..50}
 do
   mpirun ./LakeITparallel $i 2000 #00
+  echo "$i IT iteration complete"
 done
 
 cd /home/nixuser/Lake_Problem_DPS/Re-evaluation
 ./sample_parameters.sh
+echo "sample params complete"
 cd ./DPS && source /etc/profile.d/modules.sh && mpirun python resimulateDPS.py
-cd ./../Intertemporal && source /etc/profile.d/modules.sh && mpirun python resimulate.py
+echo "resimulate DPS complete"
+cd ./../Intertemporal && source /etc/profile.d/modules.sh && mpirun python resimulateIT.py
+echo "resimulate IT complete"
 cd ./.. && python calcRobustness.py
+echo "calcRobustness complete"
 
 cd ./../FigureGeneration
 python makeAllFigures.py
 
-
+echo "figures complete"
